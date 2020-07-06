775fdeaa3cc92fd50bc411d5bca66c98f9690013
1712313_NguyenTaPhuCuong <45030297+cuongcs4@users.noreply.github.com>
2020-07-06 14:10:16 +0700

    Một số màn hình mới

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,29 @@ router.get('/', function(req, res, next){
   })
 })
 
+router.get('/profile', function(req, res, next){
+  res.render('profile', {
+    title: 'Profile'
+  })
+})
+
+router.get('/table', function(req, res, next){
+  res.render('table', {
+    title: 'Table'
+  })
+})
+
+router.get('/schedule', function(req, res, next){
+  res.render('schedule', {
+    title: 'Schedule'
+  })
+})
+
 router.get('/login', function(req, res, next) {
   res.render('login', { 
     title: 'Login',
-    style: 'sb-admin-2.min.css'
+    style: 'sb-admin-2.min.css',
+    layout: false
   })
 });
 
