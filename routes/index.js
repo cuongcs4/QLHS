90f4262727e4ef7bb225c9532c79f59685d22f22
cuongcs4 <45030297+cuongcs4@users.noreply.github.com>
2020-07-16 20:06:02 +0700

    Edit thêm vài màn hình

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,60 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next){
   res.render('index', {
-    title: 'Home'
+    title: 'Home',
+    pagename: 'Home'
   })
 })
 
 router.get('/profile', function(req, res, next){
   res.render('profile', {
-    title: 'Profile'
+    title: 'Profile',
+    style: 'styleProfile.css',
+    pagename: 'Thông tin cá nhân'
   })
 })
 
-router.get('/table', function(req, res, next){
-  res.render('table', {
-    title: 'Table'
+router.get('/studenttable', function(req, res, next){
+  res.render('studentTable', {
+    title: 'Student Table',
+    style: 'styleTable.css',
+    pagename: 'Danh sách học sinh'
+  })
+})
+router.get('/classtable', function(req, res, next){
+  res.render('classTable', {
+    title: 'Class Table',
+    style: 'styleTable.css',
+    pagename: 'Danh sách lớp học'
+  })
+})
+
+router.get('/examtable', function(req, res, next){
+  res.render('examTable', {
+    title: 'Exam Table',
+    style: 'styleTable.css',
+    pagename: 'Lịch thi'
+  })
+})
+router.get('/resulttable', function(req, res, next){
+  res.render('resultTable', {
+    title: 'Kết quả học tập',
+    style: 'styleTable.css',
+    pagename: 'Kết quả học tập'
   })
 })
 
 router.get('/schedule', function(req, res, next){
   res.render('schedule', {
-    title: 'Schedule'
+    title: 'Schedule',
+    style: 'styleSchedule.css',
+    pagename: 'Thời khoá biểu'
   })
 })
 
 router.get('/login', function(req, res, next) {
   res.render('login', { 
     title: 'Login',
-    style: 'sb-admin-2.min.css',
     layout: false
   })
 });
