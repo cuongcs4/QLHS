66f8ce05e1b6d7cdbdf4fa65d49f96dbb185c67e
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-08-15 21:38:33 +0700

    fix conduct

--- Model/Class/Conduct.js
+++ Model/Class/Conduct.js
@@ -12,6 +12,8 @@ const Conduct = class {
   constructor(semester, studentID, classID, teacherID, grade) {
     this.semester = semester;
     this.studentID = studentID;
+    this.classID = classID;
+    this.teacherID = teacherID;
     this.grade = grade;
   }
 
